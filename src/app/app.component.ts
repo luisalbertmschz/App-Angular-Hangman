278ee133bf835b43382dd5b5140e63a27c6dd643
luisalbertmschz <cerebrito07@hotmail.com>
2020-06-21 21:47:32 -0400

    Creando letras de forma dinámica 🔤

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Adivinalapalabra';
+
+
+  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+  constructor() {
+
+    console.log('Se acaba de crear el App Component');
+
+  }
+
+
 }
